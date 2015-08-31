0af6334a74198d4a990c7371f1e291753844422f
Carlos Correia <carloscorreia@Carloss-MacBook-Pro.local>
2015-08-31 20:34:40 +0100

    first commit inital structure

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AEISTMobile
+//
+//  Created by Carlos Correia on 31/08/15.
+//  Copyright © 2015 AEIST. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
