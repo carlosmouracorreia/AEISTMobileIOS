2d094cad02f73a8f789da0015923544d25d2bdf9
Carlos Correia <carloscorreia@Carloss-MBP.lan>
2015-09-04 17:38:35 +0100

    progress

---
+++ RatingControl.swift
@@ -0,0 +1,17 @@
+//
+//  RatingControl.swift
+//  AEISTMobile
+//
+//  Created by Carlos Correia on 04/09/15.
+//  Copyright © 2015 AEIST. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIView {
+
+   // MARK: Initialization
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
