3f88991ccfab3cc58c38303ce3a254928670f394
Carlos Correia <carloscorreia@Carloss-MacBook-Pro.local>
2015-09-14 15:32:00 +0100

    Implementing Views

---
+++ AEISTMobile/DetailCell.swift
@@ -0,0 +1,16 @@
+//
+//  DetailCell.swift
+//  AEISTMobile
+//
+//  Created by Carlos Correia on 14/09/15.
+//  Copyright © 2015 AEIST. All rights reserved.
+//
+
+
+import UIKit
+
+class DetailCell: UITableViewCell {
+    
+    @IBOutlet weak var myTextLabel: UILabel!
+    
+}
